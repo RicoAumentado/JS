3ec38f1ac57f8955119a15ab3f980a6e39366bc4
RicoAumentado <ricoaumentaumentado2@gmail.com>
2026-03-06 22:44:38 +0800

    array

--- main.js
+++ main.js
@@ -1,8 +1,73 @@
-function checkPalindrome() {
-    let word = document.getElementById("wordInput").value.toLowerCase();
-    
-    let reverseWord = word.split("").reverse().join("");
+// let username;
+// document.getElementById(mysubmit).onclick = 
+// function (){
+//     username = document.getElementById("text").value;
+//     console.log(username);
+// }
 
-    document.getElementById("result").textContent =
-        word === reverseWord ? "Palindrome" : "Not Palindrome ";
-}
+//type of conversion
+// let age = 22;
+// let voterequired = false;
+
+// if (age >= 21){
+//     console.log("You are required to drive");
+ 
+//     if(voterequired){
+//     console.log("You are qualified to vote");
+// } else {
+//     console.log("wala lang");
+// } 
+
+// }
+// else{
+//    console.log("You must be 18 to vote") 
+// }
+// callback: 
+// function calculate(a, b, operation) {
+//     return operation(a, b);
+// }
+
+// const add = (x, y) => x + y;
+// const multiply = (x, y) => x * y;
+
+// console.log(calculate(5, 3, add));
+// console.log(calculate(5, 3, multiply));
+
+// removing:
+// const removeBtn = document.getElementById("removeBtn");
+
+// removeBtn.addEventListener("click", function() {
+
+//     if (container.lastChild) {
+//         container.removeChild(container.lastChild);
+//     }
+
+// });
+
+
+// creating:
+//  const container = document.getElementById("container");
+// const addBtn = document.getElementById("addBtn");
+
+// addBtn.addEventListener("click", function() {
+
+//     const newParagraph = document.createElement("p");
+
+//     newParagraph.textContent = "I am new here!";
+
+//     container.appendChild(newParagraph);
+
+// });
+
+
+//cvsun@1c@vr - avr
+
+
+
+//array of strings
+let firstName = ["Rico","Thania", "Dave","Adelle"];
+let LastName = ["Auditor","Villamiel","Malaran","Malaran2"];
+let ages = [22, 21, 20, 19];
+
+alert(firstName[0] + " " + LastName[0] + " is " + ages[0] + " years old.");
+
